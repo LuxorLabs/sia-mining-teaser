611099759eeba4b3439afb6ec3014d38e1622064
Eddie Wang <bonjour@eddiewang.me>
2017-09-04 16:30:09 -0500

    added docker

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,19 @@
-console.log('\'Allo \'Allo!');
+function fadeInElastic (el) {
+  TweenMax.fromTo(
+    el,
+    1.5,
+    { autoAlpha: 0, yPercent: 50 },
+    { autoAlpha: 1, yPercent: 0, ease: Back.easeOut.config(1.7) }
+  )
+}
+
+var fadeInProperties = [
+  '#hero--logo',
+  '#hero--title',
+  '#hero--subtitle',
+  '#hero--input'
+]
+
+document.addEventListener('DOMContentLoaded', function () {
+  fadeInElastic(fadeInProperties)
+})
